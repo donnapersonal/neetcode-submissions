b9f1fdbd8843cd2509d5b1d7ca9f6ae209a78f90
Donna <168793508+donnapersonal@users.noreply.github.com>
2026-07-25 21:44:24 -0700

    Add: lowest-common-ancestor-in-binary-search-tree - submission-2

---
+++ Data Structures & Algorithms/lowest-common-ancestor-in-binary-search-tree/submission-2.py
@@ -0,0 +1,46 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# class Solution:
+#     def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+#         low = min(p.val, q.val)
+#         high = max(p.val, q.val)
+#         cur = root
+#         while cur:
+#             if cur.val < low:
+#                 cur = cur.right
+#             elif cur.val > high:
+#                 cur = cur.left
+#             else:
+#                 return cur
+        
+#         return None
+
+# class Solution:
+#     def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+#         while True:
+#             if root.val > p.val and root.val > q.val:
+#                 root = root.left
+#             elif root.val < p.val and root.val < q.val:
+#                 root = root.right
+#             else:
+#                 break
+        
+#         return root
+
+class Solution:
+    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+        cur = root
+        while cur:
+            if cur.val > p.val and cur.val > q.val:
+                cur = cur.left
+            elif cur.val < p.val and cur.val < q.val:
+                cur = cur.right
+            else:
+                return cur
+        
+        return None
