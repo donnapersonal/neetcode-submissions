602f982a4afe1ed022623334d20fd2f1a84c6131
Donna <168793508+donnapersonal@users.noreply.github.com>
2026-07-25 21:55:21 -0700

    Add: lowest-common-ancestor-in-binary-search-tree - submission-7

---
+++ Data Structures & Algorithms/lowest-common-ancestor-in-binary-search-tree/submission-7.java
@@ -0,0 +1,64 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+// class Solution {
+//     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+//         int low = Math.min(p.val, q.val);
+//         int high = Math.max(p.val, q.val);
+//         TreeNode cur = root;
+//         while (cur != null) {
+//             if (cur.val < low) {
+//                 cur = cur.right;
+//             } else if (cur.val > high) {
+//                 cur = cur.left;
+//             } else {
+//                 return cur;
+//             }
+//         }
+//         return null;
+//     }
+// }
+
+// class Solution {
+//     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+//         while (true) {
+//             if (root.val > p.val && root.val > q.val) {
+//                 root = root.left;
+//             } else if (root.val < p.val && root.val < q.val) {
+//                 root = root.right;
+//             } else {
+//                 break;
+//             }
+//         }
+//         return root;
+//     }
+// }
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        TreeNode cur = root;
+        while (cur != null) {
+            if (cur.val > q.val && cur.val > p.val) {
+                cur = cur.left;
+            } else if (cur.val < q.val && cur.val < p.val) {
+                cur = cur.right;
+            } else {
+                return cur;
+            }
+        }
+        return null;
+    }
+}
