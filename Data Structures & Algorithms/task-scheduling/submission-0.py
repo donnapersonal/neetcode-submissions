914ce2180de91df85d26e78feb5355bd926186b4
Donna <168793508+donnapersonal@users.noreply.github.com>
2026-08-03 11:49:17 -0700

    Add: task-scheduling - submission-0

---
+++ Data Structures & Algorithms/task-scheduling/submission-0.py
@@ -0,0 +1,7 @@
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        task_count = Counter(tasks)
+        max_count = max(task_count.values())
+        max_count_task = sum(1 for count in task_count.values() if count == max_count)
+        min_time = (max_count - 1) * (n+1) + max_count_task
+        return max(min_time, len(tasks))
