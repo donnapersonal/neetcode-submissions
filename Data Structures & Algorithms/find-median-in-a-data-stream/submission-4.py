e8db2ff10a908953cc8c2e569a8d6b7eaebfc58c
Donna <168793508+donnapersonal@users.noreply.github.com>
2026-08-03 15:14:07 -0700

    Add: find-median-in-a-data-stream - submission-4

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-4.py
@@ -0,0 +1,40 @@
+# class MedianFinder:
+
+#     def __init__(self):
+#         self.left = []
+#         self.right = []
+
+#     def addNum(self, num: int) -> None:
+#         if len(self.left) >= len(self.right):
+#             heapq.heappush(self.left, -num)
+#             heapq.heappush(self.right, -heapq.heappop(self.left))
+#         else:
+#             heapq.heappush(self.right, num)
+#             heapq.heappush(self.left, -heapq.heappop(self.right))
+
+#     def findMedian(self) -> float:
+#         if len(self.left) > len(self.right):
+#             return -self.left[0]
+#         elif len(self.left) < len(self.right):
+#             return self.right[0]
+        
+#         return (self.right[0] - self.left[0]) / 2.0
+
+class MedianFinder:
+
+    def __init__(self):
+        self.left = []
+        self.right = []
+
+    def addNum(self, num: int) -> None:
+        if len(self.left) == len(self.right):
+            heapq.heappush(self.left, -heapq.heappushpop(self.right, num))
+        else:
+            heapq.heappush(self.right, -heapq.heappushpop(self.left, -num))
+
+    def findMedian(self) -> float:
+        if len(self.left) > len(self.right):
+            return -self.left[0]
+        
+        return (self.right[0] - self.left[0]) / 2.0
+        
