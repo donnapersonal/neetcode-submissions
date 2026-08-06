04830a83b78f171912f8732da82ca7df10cc756c
Donna <168793508+donnapersonal@users.noreply.github.com>
2026-08-06 16:00:19 -0700

    Add: jump-game-ii - submission-0

---
+++ Data Structures & Algorithms/jump-game-ii/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        n = len(nums)
+        jumps = 0
+        cur_end = 0
+        max_pos = 0
+        for i in range(n-1):
+            max_pos = max(max_pos, i + nums[i])
+            if i == cur_end:
+                jumps += 1
+                cur_end = max_pos
+
+                if cur_end >= n-1:
+                    break
+        
+        return jumps
