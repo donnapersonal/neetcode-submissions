0a8dfef5201bf1a1a33a17e8ce83a76d0041d374
Donna <168793508+donnapersonal@users.noreply.github.com>
2026-08-07 11:12:57 -0700

    Add: valid-parenthesis-string - submission-4

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-4.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        int low = 0, high = 0;
+        for (char ch : s.toCharArray()) {
+            if (ch == '(') {
+                low++;
+                high++;
+            } else if (ch == ')') {
+                low--;
+                high--;
+            } else {
+                low--;
+                high++;
+            }
+
+            if (high < 0) {
+                return false;
+            }
+            low = Math.max(low, 0);
+        }
+        return low == 0;
+    }
+}
