e8e1f7edbfc6903e1ce7c557491795f9f5742671
Donna <168793508+donnapersonal@users.noreply.github.com>
2026-08-08 14:19:46 -0700

    Add: subsets - submission-7

---
+++ Data Structures & Algorithms/subsets/submission-7.java
@@ -0,0 +1,46 @@
+// class Solution {
+//     private List<List<Integer>> res;
+//     private List<Integer> track;
+
+//     public List<List<Integer>> subsets(int[] nums) {
+//         res = new ArrayList<>();
+//         track = new ArrayList<>();
+//         backtrack(nums, 0);
+//         return res;
+//     }
+
+//     private void backtrack(int[] nums, int start) {
+//         res.add(new ArrayList<>(track));
+//         for (int i = start; i < nums.length; i++) {
+//             track.add(nums[i]);
+//             backtrack(nums, i+1);
+//             track.remove(track.size() - 1);
+//         }
+//     }
+// }
+
+class Solution {
+    private List<List<Integer>> res;
+    private List<Integer> track;
+
+    public List<List<Integer>> subsets(int[] nums) {
+        res = new ArrayList<>();
+        track = new ArrayList<>();
+        
+        backtrack(nums, 0);
+        return res;
+    }
+
+    private void backtrack(int[] nums, int index) {
+        if (index == nums.length) {
+            res.add(new ArrayList<>(track));
+            return;
+        }
+
+        backtrack(nums, index+1);
+
+        track.add(nums[index]);
+        backtrack(nums, index+1);
+        track.remove(track.size() - 1);
+    }
+}
