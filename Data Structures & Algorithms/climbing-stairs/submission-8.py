e817268fdc42b597247ac48a9762f6c91b0efe25
Donna <168793508+donnapersonal@users.noreply.github.com>
2026-08-23 18:16:08 -0700

    Add: climbing-stairs - submission-8

---
+++ Data Structures & Algorithms/climbing-stairs/submission-8.py
@@ -0,0 +1,38 @@
+# class Solution:
+#     def climbStairs(self, n: int) -> int:
+#         self.memo = [0] * (n+1);
+#         return self.dp(n);
+    
+#     def dp(self, n):
+#         if n <= 2:
+#             return n
+        
+#         if self.memo[n] > 0:
+#             return self.memo[n]
+        
+#         self.memo[n] = self.dp(n-1) + self.dp(n-2)
+#         return self.memo[n]
+
+# class Solution:
+#     def climbStairs(self, n: int) -> int:
+#         dp = [0 for _ in range(n+1)]
+#         dp[0] = 1
+#         dp[1] = 1
+#         for i in range(2, n+1):
+#             dp[i] = dp[i-1] + dp[i-2]
+        
+#         return dp[n]
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 2:
+            return n
+        
+        prev1, prev2 = 2, 1
+        for i in range(3, n+1):
+            cur = prev1 + prev2
+            prev2 = prev1
+            prev1 = cur
+        
+        return prev1
+
