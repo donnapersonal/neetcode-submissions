1dda1ee088569f2ca8d6295db59cf528d41d3715
Donna <168793508+donnapersonal@users.noreply.github.com>
2026-08-23 18:07:18 -0700

    Add: climbing-stairs - submission-4

---
+++ Data Structures & Algorithms/climbing-stairs/submission-4.java
@@ -0,0 +1,15 @@
+class Solution {
+    private int[] memo;
+    public int climbStairs(int n) {
+        memo = new int[n+1];
+        // Arrays.fill(memo, 0);
+        return dp(n);
+    }
+
+    private int dp(int n) {
+        if (n <= 2) return n;
+        if (memo[n] > 0) return memo[n];
+        memo[n] = dp(n-1) + dp(n-2);
+        return memo[n];
+    }
+}
