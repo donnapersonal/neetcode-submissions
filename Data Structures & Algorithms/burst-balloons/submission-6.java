600e18a2ab46186ab839d88b1667c89b278adade
Donna <168793508+donnapersonal@users.noreply.github.com>
2026-09-16 18:17:20 -0700

    Add: burst-balloons - submission-6

---
+++ Data Structures & Algorithms/burst-balloons/submission-6.java
@@ -0,0 +1,52 @@
+// class Solution {
+//     public int maxCoins(int[] nums) {
+//         int m = nums.length;
+//         int[] val = new int[m+2];
+//         val[0] = 1;
+//         val[m+1] = 1;
+
+//         for (int i = 0; i < m; i++) {
+//             val[i+1] = nums[i];
+//         }
+
+//         int n = val.length;
+//         int[][] dp = new int[n][n];
+//         for (int len = 2; len < n; len++) {
+//             for (int left = 0; left < n - len; left++) {
+//                 int right = left + len;
+//                 for (int k = left+1; k < right; k++) {
+//                     int coins = dp[left][k] + val[left] * val[k] * val[right] + dp[k][right];
+//                     dp[left][right] = Math.max(dp[left][right], coins);
+//                 }
+//             }
+//         }
+
+//         return dp[0][n-1];
+//     }
+// }
+
+class Solution {
+    public int maxCoins(int[] nums) {
+        int m = nums.length;
+        int[] val = new int[m+2];
+        val[0] = 1;
+        val[m+1] = 1;
+
+        for (int i = 0; i < m; i++) {
+            val[i+1] = nums[i];
+        }
+
+        int n = val.length;
+        int[][] dp = new int[n][n];
+        for (int left = n-2; left >= 0; left--) {
+            for (int right = left+2; right < n; right++) {
+                for (int k = left+1; k < right; k++) {
+                    int coins = dp[left][k] + val[left] * val[k] * val[right] + dp[k][right];
+                    dp[left][right] = Math.max(dp[left][right], coins);
+                }
+            }
+        }
+
+        return dp[0][n-1];
+    }
+}
